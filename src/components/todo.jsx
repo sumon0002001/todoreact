6fdd2a3648b1afacc85bb2fdbc39dd35a67492be
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-06-05 23:09:57 +0600

    Add function to delete buttons

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,18 +2,30 @@ import React, {useState} from 'react';
 import image from '../images/images.png';
 
 const Todo = () => {
-    const [input, setInput] = useState("");
-    const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
 
 
    const changeHandler = (e) => {
       let newInput = e.target.value
-      setInput([newInput]);
+      setInput(newInput);
     }
 
     const clickHandler = () => {
-        setItems(input)
-        setInput("")
+        if(!input) {
+            alert("no data")
+        } else {
+            setItems([...items, input])
+            setInput("")
+        }
+    }
+
+    const delHandler = (id) => {
+        console.log(id);
+        const updatedValue = items.filter((ele, ind) => {
+            return ind !== id;
+        })
+        setItems(updatedValue)
 
     }
 
@@ -42,10 +54,19 @@ const Todo = () => {
             ></i>
           </div>
           <div className="showItems">
-            <div className="eachItem">
-              <h3>Apple</h3>
-              <i className= "far fa-trash-alt add-btn" title="Delete items"></i>
+           {items.map((elem, ind) => {
+             return (
+                <div className="eachItem" key = {ind}>
+              <h3>{elem}</h3>
+              <i className= "far fa-trash-alt add-btn" 
+                 title="Delete items"
+                 onClick = {()=>delHandler(ind)}
+              >
+              </i>
             </div>
+             )
+           })}
+           
           </div>
           <div className="showItems">
             <button className="btn effect04" data-sm-link-text="Remove All"><span>CHECK LIST</span></button>
